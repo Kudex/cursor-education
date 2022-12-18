d32294083d9859df1eafdc3670c38a660b7c4227
Kidron-Lisovskiy Yevhen <Kidronkudex@gmail.com>
2022-12-18 05:53:23 +0200

    made refactoring of hm_02

--- hm_02/js/script.js
+++ hm_02/js/script.js
@@ -1,29 +1,40 @@
 const wrap = document.querySelector(".wrapper");
 
-const n = Number(prompt("Введіть ціле число від якого будемо рахувати"));
-console.log(n);
-const m = Number(prompt("Введіть  ціле число до якого будемо рахувати"));
-console.log(m);
-if (Number.isInteger(n) && Number.isInteger(m)) {
-  const boo = confirm("Чи треба пропустити парні числа?");
-  let result = 0;
-  for (let i = n; i <= m; i++) {
-    if (
-      Number.isInteger(n) &&
-      Number.isInteger(m) &&
-      boo === true &&
-      i % 2 !== 0
-    ) {
-      result += i;
-      wrap.innerHTML = `Результат: ${result}`;
-      console.log(i);
-    } else if (Number.isInteger(n) && Number.isInteger(m) && boo === false) {
-      result += i;
-      wrap.innerHTML = `Результат: ${result}`;
-    } else {
-      continue;
-    }
+let firstNum = 0;
+let secondNum = 0;
+
+do {
+  firstNum = Number(prompt("Введіть ціле число від якого будемо рахувати"));
+  if (!Number.isInteger(firstNum) || firstNum <= 0) {
+    alert("Перше число не є цілим або менше одиниці. Введіть коректні данні");
   }
-} else {
-  console.log("N або M не ціле число");
+} while (!Number.isInteger(firstNum) || firstNum <= 0);
+
+do {
+  secondNum = Number(
+    prompt(
+      "Введіть ціле число до якого будемо рахувати, воно має бути більшим за перше число"
+    )
+  );
+  if (!Number.isInteger(secondNum) || secondNum <= firstNum) {
+    alert(
+      "Перше число не є цілим, меншим або дорівнює. Введіть коректні данні"
+    );
+  }
+} while (!Number.isInteger(secondNum) || secondNum <= firstNum);
+
+const boo = confirm("Чи треба пропустити парні числа?");
+
+let result = 0;
+for (let i = firstNum; i <= secondNum; i++) {
+  if (boo === true && i % 2 !== 0) {
+    result += i;
+    wrap.innerHTML = `Результат: ${result}`;
+    console.log(i);
+  } else if (boo === false) {
+    result += i;
+    wrap.innerHTML = `Результат: ${result}`;
+  } else {
+    continue;
+  }
 }
