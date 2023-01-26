30619cf8f1225b176f416a90e8e0ba682c7591e5
Kidron-Lisovskiy Yevhen <Kidronkudex@gmail.com>
2023-01-26 03:21:10 +0200

    hw_12 DONE

--- hw_12/script.js
+++ hw_12/script.js
@@ -1,17 +1,55 @@
 const STAR_WARS_URL = "https://swapi.dev/api/";
 const input = document.querySelector(".inputFilm");
-
+let getPlanet = document.querySelector(".mainwrapper");
+const container = document.querySelector(".container");
+const btnStart = document.querySelector(".getInfo");
 let arr2 = [];
 let arrOfPlanets = [];
 let filmPath = input.value;
+let url = `/films/${filmPath}/`;
+
 input.addEventListener("input", (event) => {
   filmPath = event.data;
 });
+getPlanet = addEventListener("click", function (el) {
+  const { target } = el;
+  if (target.innerText == "NEXT>>>") {
+    container.innerHTML = "";
+    arrOfPlanets.forEach((el) => {
+      renderSinglePlanet(el);
+    });
+  }
+});
 
-const container = document.querySelector(".container");
-const btnStart = document.querySelector(".getInfo");
+function renderSinglePlanet(planet) {
+  console.log("renderSinglePlanet(planet): ", planet);
+  const userWrapper = document.createElement("div");
+  userWrapper.classList.add("user");
+  const dataWrapper = document.createElement("div");
+  dataWrapper.classList.add("dataWrapper");
+  console.log("Planets user.name : ", planet.name);
+  userWrapper.style.backgroundImage = `url(src/${planet.name
+    .split(" ")
+    .join("")}.jpg)`;
+  const planetName = document.createElement("h2");
+  planetName.classList.add("user__name");
+  planetName.textContent = planet.name;
+  const diameter = document.createElement("h3");
+  diameter.classList.add("user__birthday");
+  diameter.textContent = `D: ${planet.diameter}`;
+  const rotation_period = document.createElement("h4");
+  rotation_period.classList.add("user__gender");
+  rotation_period.textContent = `Period: ${planet.rotation_period}`;
+  userWrapper.append(planetName);
+  userWrapper.append(dataWrapper);
+  dataWrapper.append(diameter);
+  dataWrapper.append(rotation_period);
+  container.append(userWrapper);
+}
+
 btnStart.addEventListener("click", () => {
-  getUsers(`/films/${filmPath}/`);
+  url = `/films/${filmPath}/`;
+  getUsers(STAR_WARS_URL, url);
 });
 
 const btnNext = document.querySelector(".next");
@@ -43,7 +81,7 @@ function renderSingleUser(user) {
   container.append(userWrapper);
 }
 
-async function getUsers(url) {
+async function getUsers(STAR_WARS_URL, url) {
   container.innerHTML = "";
   arr2 = [];
   arrOfPlanets = [];
@@ -56,7 +94,6 @@ async function getUsers(url) {
     const person = await request_2.json();
     arr2.push(person);
   }
-
   for (const url of planets) {
     const request_planets = await fetch(`${url}`);
     const planetURL = await request_planets.json();
